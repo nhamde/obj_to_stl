fe129d2d52aee5cb78c00cff1f87fbd4a643752c
nhamde <narayan.hamde@cctech.co.in>
2024-10-25 15:37:14 +0530

    i am king

--- OBJReader.cpp
+++ OBJReader.cpp
@@ -15,38 +15,59 @@ void OBJReader::read(const string& fileName, Triangulation& triangulation)
     map<double, int> uniqueMap;
     double xyz[3];
     vector<int> pointIndices;
+    vector<Point> oPoints;
 
     ifstream infile(fileName);
     if (infile.is_open())
     {
-       string line;
-        
-    while (getline(infile, line))
-    {
-        stringstream ss(line);
-        string word;
-        while (ss >> word)
-        {
-            if (word == "v")
-            {
-                ss >> xyz[0] >> xyz[1] >> xyz[2];
+        string line;
 
-                for (int i = 0; i < 3; i++)
+        while (getline(infile, line))
+        {
+            stringstream ss(line);
+            string word;
+            while (ss >> word)
+            {
+                if (word == "v")
                 {
-                    auto pair = uniqueMap.find(xyz[i]);
-                    if (pair == uniqueMap.end())
+                    ss >> xyz[0] >> xyz[1] >> xyz[2];
+
+                    for (int i = 0; i < 3; i++)
                     {
-                       triangulation.uniqueValues.push_back(xyz[i]);
-                        uniqueMap[xyz[i]] = triangulation.uniqueValues.size() - 1;
-                        pointIndices.push_back(triangulation.uniqueValues.size() - 1);
+                        auto pair = uniqueMap.find(xyz[i]);
+                        if (pair == uniqueMap.end())
+                        {
+                            triangulation.uniqueValues.push_back(xyz[i]);
+                            uniqueMap[xyz[i]] = triangulation.uniqueValues.size() - 1;
+                            pointIndices.push_back(triangulation.uniqueValues.size() - 1);
+                        }
+                        else
+                        {
+                            pointIndices.push_back(pair->second);
+                        }
                     }
-                    else
-                    {
-                        pointIndices.push_back(pair->second);
-                    }
+                }
+
+                if (word == "f")
+                {
+                    ss >> xyz[0] >> xyz[1] >> xyz[2];
+
+                    cout << xyz[0] << " " << xyz[1] << " " << xyz[2] << endl;
                 }
             }
         }
-    }
+        for (int i = 0; i < pointIndices.size(); i += 3)
+        {
+
+            if (i + 2 < pointIndices.size())
+            {
+                int x = pointIndices[i];
+                int y = pointIndices[i + 1];
+                int z = pointIndices[i + 2];
+
+                oPoints.push_back(Point(x, y, z)); 
+            }
+        }
+
     }
 }
